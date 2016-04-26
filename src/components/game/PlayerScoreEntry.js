7d8e7be2c74fd03e1d212b7700374cf59822406d
Joni Nevalainen <joni.nevalainen@gofore.com>
2016-04-26 21:23:58 +0300

    do not allow scores less than 1

--- src/components/game/PlayerScoreEntry.js
+++ src/components/game/PlayerScoreEntry.js
@@ -4,16 +4,26 @@ import React, { Component } from 'react'
 export default class PlayerScoreEntry extends Component {
     render() {
         const p = this.props.player;
+        const score = p.scores[this.props.currentHole];
         const totalScore = _.sum(p.scores);
         return (
             <tr>
                 <td>{p.name}</td>
                 <td>
-                    <button class="btn btn-default" onClick={this.props.onScoreDecreased.bind(this, p)}>-</button>
+                    <button
+                        class="btn btn-default"
+                        onClick={this.props.onScoreDecreased.bind(this, p)}
+                        disabled={score <= 1}>
+                        -
+                    </button>
                 </td>
-                <td>{p.scores[this.props.currentHole]}</td>
+                <td>{score}</td>
                 <td>
-                    <button class="btn btn-default" onClick={this.props.onScoreIncreased.bind(this, p)}>+</button>
+                    <button
+                        class="btn btn-default"
+                        onClick={this.props.onScoreIncreased.bind(this, p)}>
+                        +
+                    </button>
                 </td>
                 <td>({ totalScore })</td>
             </tr>
